4335b03cebde08f2d4fe0803211bb3bd6f97e841
Luis Antonio Zambrano Demera <76109631+lzambran@users.noreply.github.com>
2023-05-18 20:03:36 -0500

    primer

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: 'variables.env'});
+
+
+
+// Cors permite que un cliente se conecta a otro servidor para el intercambio de recursos
+
+const cors = require('cors');
+
+// conectar a mongose
+// mongoose.Promise = global.Promise;
+// mongoose.connect("mongodb://localhost/migracion", console.log('DATABSE CONNECTADO...'), {
+//     useNewUrlParser: true
+// });
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URL, console.log('DATABSE CONNECTADO...'), {
+    useNewUrlParser: true
+});
+
+
+// crear el servidor 
+const app = express();
+
+// habilitar bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Definir un dominio 
+const whitelist = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: (origin, callback) => {
+        const existe = whitelist.some( dominio => dominio === origin);
+        if (existe){
+            callback(null, true);
+        }else{
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
+
+// Habilitar cors
+app.use(cors(corsOptions));
+// app.use(cors());
+
+// Rutas de la app
+app.use('/', routes());
+
+// carpeta publica
+app.use(express.static('uploads'));
+
+const host =process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 5000;
+
+// puerto
+app.listen(port, host, () => {
+    console.log("el servidor esta funcionando");
+})
